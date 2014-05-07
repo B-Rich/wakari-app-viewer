0c076f73a1dc70ee6e2b5911181c638fb7c6426c
srossross <srossross@gmail.com>
2014-05-07 13:09:50 -0500

    FIX: nbviewer caching issue
    fixes ContinuumIO/wakari-issues#13

--- wkviewer/renderer.py
+++ wkviewer/renderer.py
@@ -13,16 +13,17 @@ from pygments.util import ClassNotFound
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 import mimetypes
+import os
 
 
 def raw_renderer(full_path):
     g = open(full_path)
     res = current_app.response_class(g, direct_passthrough=True)
     mimetype, _ = mimetypes.guess_type(full_path)
-    
+
     if mimetype is None:
         mimetype = 'application/octet-stream'
-        
+
     res.headers['Content-Type'] = mimetype
     return res
 
@@ -37,33 +38,39 @@ def nb_renderer(full_path):
                 return raw_renderer(cache_file)
         except:
             current_app.logger.warn('There was an error reading from the cache file')
-    
+
     ex = HTMLExporter(extra_loaders=[current_app.jinja_env.loader],
                       template_file='wakari_notebook.html')
-    
+
     ex.environment.globals.update(current_app.jinja_env.globals)
     current_app.update_template_context(ex.environment.globals)
     ex.environment.globals.update(dirname=dirname(request.view_args['path']))
-    
+
     output, _ = ex.from_filename(full_path)
-    
+
+
     try:
         with open(cache_file, 'w') as fd:
             current_app.logger.debug('Writing Cache File')
-            fd.write(output)
-    except:
+            fd.write(output.encode(errors='replace'))
+    except OSError:
         current_app.logger.warn('There was an error writing to the cache file')
-        
+        try:
+            if isfile(cache_file): os.unlink(cache_file)
+        except OSError:
+            current_app.logger.warn('There was an removing cache file')
+            pass
+
     return output
-    
+
 def pygments_renderer(full_path):
     lexer = get_lexer_for_filename(full_path)
-    formatter = HtmlFormatter(linenos=True, 
+    formatter = HtmlFormatter(linenos=True,
                               anchorlinenos=True,
                               cssclass="source")
     with open(full_path) as fd:
         code = highlight(fd.read(), lexer, formatter)
-        
+
     return render_template('code.html', code=code,
                            up=dirname(request.view_args['path']),
                            name=basename(full_path),
@@ -75,8 +82,8 @@ def get_renderer(full_path):
     else:
         try:
             get_lexer_for_filename(full_path)
-            return pygments_renderer 
+            return pygments_renderer
         except ClassNotFound:
             return raw_renderer
-        
+
     return raw_renderer
